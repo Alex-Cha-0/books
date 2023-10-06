1847c0ad4ab314c065273bdd9d5cf37d5ba8e652
alex <spb.roout@gmail.com>
2023-10-06 15:03:14 +0300

    add rating system and permissions

--- store/tests/test_api.py
+++ store/tests/test_api.py
@@ -1,14 +1,19 @@
+import json
+
+from django.contrib.auth.models import User
 from django.urls import reverse
 from rest_framework import status
+from rest_framework.exceptions import ErrorDetail
 from rest_framework.test import APITestCase
 
-from store.models import Book
+from store.models import Book, UserBookRelation
 from store.serializers import BooksSerializer
 
 
 class BooksApiTestCase(APITestCase):
     def setUp(self):
-        self.book_1 = Book.objects.create(name='Test book 1', price=25, author_name='Author 1')
+        self.user = User.objects.create(username='test_username')
+        self.book_1 = Book.objects.create(name='Test book 1', price=25, author_name='Author 1', owner=self.user)
         self.book_2 = Book.objects.create(name='Test book 2', price=242, author_name='Author 2')
         self.book_3 = Book.objects.create(name='Test book Author 1', price=25, author_name='Author 3')
 
@@ -45,6 +50,190 @@ class BooksApiTestCase(APITestCase):
         self.assertEqual(serializer_data, response.data)
 
     def test_get_name_models(self):
-        print(self.book_1)
-        print(self.book_1.__str__())
         self.assertEqual(str(self.book_1), self.book_1.__str__())
+
+    def test_create(self):
+        self.assertEqual(3, Book.objects.all().count())
+        url = reverse('book-list')
+        data = {"name": "Programming in Python 3.12",
+                "price": 231,
+                "author_name": "Alex Chavlytko"}
+        json_data = json.dumps(data)
+        # Обход авторизации для пост запроса
+        self.client.force_login(self.user)
+        response = self.client.post(url, data=json_data, content_type='application/json')
+        # Сравниваем что пришло с запроса и с сериалайзера
+
+        self.assertEqual(status.HTTP_201_CREATED, response.status_code)
+        self.assertEqual(4, Book.objects.all().count())
+        self.assertEqual(self.user, Book.objects.last().owner)
+
+    def test_update(self):
+        url = reverse('book-detail', args=(self.book_1.id,))
+        data = {"name": self.book_1.name,
+                "price": 555,
+                "author_name": self.book_1.author_name}
+        json_data = json.dumps(data)
+        # Обход авторизации для пут запроса
+        self.client.force_login(self.user)
+        response = self.client.put(url, data=json_data, content_type='application/json')
+        # Сравниваем что пришло с запроса и с сериалайзера
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+        self.book_1.refresh_from_db()
+        self.assertEqual(555, self.book_1.price)
+
+    def test_update_not_owner(self):
+        self.user2 = User.objects.create(username='test_username2')
+        url = reverse('book-detail', args=(self.book_1.id,))
+        data = {"name": self.book_1.name,
+                "price": 555,
+                "author_name": self.book_1.author_name}
+        json_data = json.dumps(data)
+        # Обход авторизации для пут запроса
+        self.client.force_login(self.user2)
+        response = self.client.put(url, data=json_data, content_type='application/json')
+        # Сравниваем что пришло с запроса и с сериалайзера
+        self.assertEqual(status.HTTP_403_FORBIDDEN, response.status_code)
+        self.assertEqual({'detail': ErrorDetail(string='You do not have permission to perform this action.',
+                                                code='permission_denied')}, response.data)
+        self.book_1.refresh_from_db()
+        self.assertEqual(25, self.book_1.price)
+
+    def test_update_not_owner_but_staff(self):
+        self.user2 = User.objects.create(username='test_username2', is_staff=True)
+        url = reverse('book-detail', args=(self.book_1.id,))
+        data = {"name": self.book_1.name,
+                "price": 555,
+                "author_name": self.book_1.author_name}
+        json_data = json.dumps(data)
+        # Обход авторизации для пут запроса
+        self.client.force_login(self.user2)
+        response = self.client.put(url, data=json_data, content_type='application/json')
+        # Сравниваем что пришло с запроса и с сериалайзера
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+
+        self.book_1.refresh_from_db()
+        self.assertEqual(555, self.book_1.price)
+
+    def test_delete(self):
+
+        url = reverse('book-detail', args=(self.book_1.id,))
+        data = {"id": self.book_1.id}
+        json_data = json.dumps(data)
+        # Обход авторизации для delete запроса
+        self.client.force_login(self.user)
+        response = self.client.delete(url, data=json_data, content_type='application/json')
+        # Сравниваем что пришло с запроса
+        self.assertEqual(status.HTTP_204_NO_CONTENT, response.status_code)
+
+        def book_exist(book):
+            try:
+                book.refresh_from_db()
+            except:
+                return False
+
+        self.assertEqual(book_exist(self.book_1), Book.objects.filter(id=self.book_1.id).exists())
+
+    def test_delete_not_owner(self):
+        self.user2 = User.objects.create(username='test_username2')
+        url = reverse('book-detail', args=(self.book_1.id,))
+        data = {"id": self.book_1.id}
+        json_data = json.dumps(data)
+        # Обход авторизации для delete запроса
+        self.client.force_login(self.user2)
+        response = self.client.delete(url, data=json_data, content_type='application/json')
+        # Сравниваем что пришло с запроса
+        self.assertEqual(status.HTTP_403_FORBIDDEN, response.status_code)
+
+        def book_exist(book):
+            try:
+                book.refresh_from_db()
+            except:
+                return False
+
+    def test_delete_not_owner_but_staff(self):
+        self.user2 = User.objects.create(username='test_username2', is_staff=True)
+        url = reverse('book-detail', args=(self.book_1.id,))
+        data = {"id": self.book_1.id}
+        json_data = json.dumps(data)
+        # Обход авторизации для delete запроса
+        self.client.force_login(self.user2)
+        response = self.client.delete(url, data=json_data, content_type='application/json')
+        # Сравниваем что пришло с запроса
+        self.assertEqual(status.HTTP_204_NO_CONTENT, response.status_code)
+
+        def book_exist(book):
+            try:
+                book.refresh_from_db()
+            except:
+                return False
+
+        self.assertEqual(book_exist(self.book_1), Book.objects.filter(id=self.book_1.id).exists())
+
+
+class BooksRelationTestCase(APITestCase):
+    def setUp(self):
+        self.user = User.objects.create(username='test_username')
+        self.user2 = User.objects.create(username='test_username2')
+        self.book_1 = Book.objects.create(name='Test book 1', price=25, author_name='Author 1', owner=self.user)
+        self.book_2 = Book.objects.create(name='Test book 2', price=242, author_name='Author 2')
+
+    def test_like(self):
+        url = reverse('userbookrelation-detail', args=(self.book_1.id,))
+
+        data = {"like": True,
+                }
+        json_data = json.dumps(data)
+        self.client.force_login(self.user)
+        """При пут передается весь объект, а при патч передается одно поле"""
+        response = self.client.patch(url, data=json_data, content_type='application/json')
+        # Сравниваем что пришло с запроса и с сериалайзера
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+        relation = UserBookRelation.objects.get(user=self.user,
+                                                book=self.book_1)
+        self.assertTrue(relation.like)
+
+        data = {"in_bookmarks": True,
+                }
+        json_data = json.dumps(data)
+        response = self.client.patch(url, data=json_data, content_type='application/json')
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+        relation = UserBookRelation.objects.get(user=self.user,
+                                                book=self.book_1)
+        self.assertTrue(relation.in_bookmarks)
+
+    def test_rate(self):
+        url = reverse('userbookrelation-detail', args=(self.book_1.id,))
+
+        data = {"rate": 3,
+                }
+        json_data = json.dumps(data)
+        self.client.force_login(self.user)
+        """При пут передается весь объект, а при патч передается одно поле"""
+        response = self.client.patch(url, data=json_data, content_type='application/json')
+
+        # Сравниваем что пришло с запроса и с сериалайзера
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+
+        relation = UserBookRelation.objects.get(user=self.user,
+                                                book=self.book_1)
+        self.assertEqual(3, relation.rate)
+
+    def test_rate_worng(self):
+        url = reverse('userbookrelation-detail', args=(self.book_1.id,))
+
+        data = {"rate": 6,
+                }
+        json_data = json.dumps(data)
+        self.client.force_login(self.user)
+        """При пут передается весь объект, а при патч передается одно поле"""
+        response = self.client.patch(url, data=json_data, content_type='application/json')
+
+        # Сравниваем что пришло с запроса и с сериалайзера
+        self.assertEqual(status.HTTP_400_BAD_REQUEST, response.status_code, response.data)
+
+        relation = UserBookRelation.objects.get(user=self.user,
+                                                book=self.book_1)
+
+
+
--- store/tests/test_serializer.py
+++ store/tests/test_serializer.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
 
 from store.models import Book
@@ -6,22 +7,27 @@ from store.serializers import BooksSerializer
 
 class BooksSerialiserTEstCase(TestCase):
     def test_get(self):
-        book_1 = Book.objects.create(name='Test book 1', price=25, author_name='Author 1')
-        book_2 = Book.objects.create(name='Test book 2', price=242, author_name='Author 2')
+        self.user = User.objects.create(username='test_username')
+        book_1 = Book.objects.create(name='Test book 1', price=25, author_name='Author 1', owner=self.user)
+        book_2 = Book.objects.create(name='Test book 2', price=242, author_name='Author 2', owner=self.user)
         data = BooksSerializer([book_1, book_2], many=True).data
         # Данные которые мы ожидаем!
         expected_data = [
             {
                 'id': book_1.id,
                 'name': 'Test book 1',
-                'price': '25.00',
+                'price': 25.00,
                 'author_name': 'Author 1',
+                'owner': 'test_username',
             },
             {
                 'id': book_2.id,
                 'name': 'Test book 2',
-                'price': "242.00",
+                'price': 242.00,
                 'author_name': 'Author 2',
+                'owner': 'test_username',
             }
         ]
+        print(expected_data)
+        print(data)
         self.assertEqual(expected_data, data)
